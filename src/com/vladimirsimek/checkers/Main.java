9de6ad7302980c54342e76ce465eacc84604016c
vladimir-simek <minervladik@gmail.com>
2021-01-12 23:31:00 +0100

    Default interface

--- src/com/vladimirsimek/checkers/Main.java
+++ src/com/vladimirsimek/checkers/Main.java
@@ -1,8 +1,56 @@
 package com.vladimirsimek.checkers;
 
+import java.util.Scanner;
+
+import static com.vladimirsimek.checkers.gui.clearScreen;
+import static com.vladimirsimek.checkers.gui.drawFields;
+
 public class Main {
 
+    public static final String blackspot = "■";
+    public static final String whitespot = "□";
+    public static final String player1 = "©";
+    public static final String player2 = "®";
+    public static final String row = "-----------------------------------";
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String[][] fields = {
+                {whitespot,player1,whitespot,player1,whitespot,player1,whitespot,player1},
+                {player1,whitespot,player1,whitespot,player1,whitespot,player1,whitespot},
+                {whitespot,player1,whitespot,player1,whitespot,player1,whitespot,player1},
+                {blackspot,whitespot,blackspot,whitespot,blackspot,whitespot,blackspot,whitespot},
+                {whitespot,blackspot,whitespot,blackspot,whitespot,blackspot,whitespot,blackspot},
+                {player2,whitespot,player2,whitespot,player2,whitespot,player2,whitespot},
+                {whitespot,player2,whitespot,player2,whitespot,player2,whitespot,player2},
+                {player2,whitespot,player2,whitespot,player2,whitespot,player2,whitespot}
+        };
+        drawFields(fields);
+
+        int player1amount = 1;
+        int player2amount = 1;
+
+        while (player1amount != 0 || player2amount != 0) {
+            player1amount = 0;
+            player2amount = 0;
+            for (String[] field : fields) { // Checks if there are still some players left
+                for (String s : field) {
+                    if (s.equals(player1)) {
+                        player1amount++;
+                    } else if (s.equals(player2)) {
+                        player2amount++;
+                    }
+                }
+            } if (player1amount == 0 || player2amount == 0) { // If someone lost all checkers the game will end
+                clearScreen();
+                System.out.println("GGWP!");
+            }
+            System.out.println(row);
+
+
+        }
+        clearScreen();
+        System.out.println("GGWP!");    
+
     }
 }
--- src/com/vladimirsimek/checkers/gui.java
+++ src/com/vladimirsimek/checkers/gui.java
@@ -1,4 +1,21 @@
 package com.vladimirsimek.checkers;
 
 public class gui {
+
+    public static void drawFields(String[][] fields) {
+        /*
+        Draws the playboard.
+         */
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields[i].length; j++) {
+                System.out.print(fields[i][j] + " ");
+            }
+            System.out.println();
+        }
+    } // Draws the game playground
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    } // Should clear screen
 }
